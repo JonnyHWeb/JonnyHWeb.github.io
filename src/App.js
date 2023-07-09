ac553401f8bc7c83c42805346e35f9f284b23970
Jonny Hunter <hunter.jonny@yahoo.co.uk>
2023-07-09 16:58:32 +0100

    Basic get and display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss'
+import axios from 'axios';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then(response => {
+        setPhotos(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {photos.map(photo => (
+        <li key={photo.id}><img src={photo.thumbnailUrl} alt="{photo.title}" loading="lazy"></img> </li>
+      ))}
+    </ul>
   );
 }
 
