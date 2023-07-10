7b139c9e707b7198753253c430b19e021781ea83
Jonny Hunter <hunter.jonny@yahoo.co.uk>
2023-07-10 18:17:54 +0100

    add sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss'
 import axios from 'axios';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 function App() {
+
   const [photos, setPhotos] = useState([]);
 
+  Modal.setAppElement('#root');
+
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/photos')
+    axios.get('https://jsonplaceholder.typicode.com/photos?_limit=100')
       .then(response => {
         setPhotos(response.data);
       })
@@ -15,10 +20,16 @@ function App() {
       });
   }, []);
 
+
+
   return (
-    <ul>
+    <ul className="cards">
       {photos.map(photo => (
-        <li key={photo.id}><img src={photo.thumbnailUrl} alt="{photo.title}" loading="lazy"></img> </li>
+        <li className="card" key={photo.id}>
+          <img className="card__img" src={photo.thumbnailUrl} alt={photo.title} loading="lazy"></img>
+          <h2 className="card__title">{photo.title}</h2> 
+          <span className="card__page-number">{photo.id}</span>
+        </li>
       ))}
     </ul>
   );
