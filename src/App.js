2acda96ad9d2d018b3a75a8259410ac8abcd3500
Jonny Hunter <hunter.jonny@yahoo.co.uk>
2023-07-11 16:35:20 +0100

    modal base

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { useState, useEffect } from 'react';
 import './App.scss'
 import axios from 'axios';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 
 function App() {
 
   const [photos, setPhotos] = useState([]);
 
-  Modal.setAppElement('#root');
+  const displayModal = (url) => {
+
+    // Create modal to display JSON url
+    var section = document.createElement("section");
+    section.classList.add("modal");
+    var img = document.createElement("img");
+    img.classList.add("modal__img");
+    
+    img.src = (url);
+    section.appendChild(img)
+
+    // Show Modal
+    var root = document.getElementById("root");
+    root.appendChild(section)
+
+    // Add close btn which deletes modal 
+    var btn = document.createElement("button");
+    btn.classList.add("modal__btn");
+    section.appendChild(btn)
+    btn.innerHTML = "Close";
+  }
 
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/photos?_limit=100')
@@ -20,15 +39,15 @@ function App() {
       });
   }, []);
 
-
-
   return (
     <ul className="cards">
       {photos.map(photo => (
         <li className="card" key={photo.id}>
-          <img className="card__img" src={photo.thumbnailUrl} alt={photo.title} loading="lazy"></img>
-          <h2 className="card__title">{photo.title}</h2> 
-          <span className="card__page-number">{photo.id}</span>
+          <a className="card__link" href="#" onClick={displayModal.bind(this, photo.url)}>
+            <img className="card__img" src={photo.thumbnailUrl} alt={photo.title} loading="lazy"></img>
+            <h2 className="card__title">{photo.title}</h2> 
+            <span className="card__page-number">{photo.id}</span>
+          </a>
         </li>
       ))}
     </ul>
